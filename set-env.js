31ea39d7c36736ebe6ce61c06469df522bf4bee8
Alfonso <alfonsogaitanperez@gmail.com>
2025-11-27 14:29:37 +0100

    Last expected

---
+++ set-env.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const targetPath = './src/environments/environment.prod.ts';
+
+const envFileContent = `
+export const environment = {
+  production: true,
+  apiKey: '${process.env.NG_APP_API_KEY}'
+};
+`;
+
+fs.writeFileSync(targetPath, envFileContent);
